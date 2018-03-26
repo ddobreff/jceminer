a09469517f24d15c0b2b341170de0fa2b123ba0b
Jean Cyr <jean.m.cyr@gmail.com>
2018-03-26 16:47:08 -0400

    HTML table format

--- libapi/http/httpServer.cpp
+++ libapi/http/httpServer.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include <thread>
 #include <mongoose/mongoose.h>
+#include <unistd.h>
+#include <limits.h>
 #include "httpServer.h"
 #include "libdevcore/Log.h"
 #include "libdevcore/Common.h"
@@ -11,15 +13,43 @@ using namespace eth;
 
 httpServer http_server;
 
+
 void httpServer::getstat1(stringstream& ss)
 {
+	char hostName[HOST_NAME_MAX + 1];
+	gethostname(hostName, HOST_NAME_MAX + 1);
 	using namespace std::chrono;
 	auto info = miner_get_buildinfo();
 	WorkingProgress p = m_farm->miningProgress();
 	SolutionStats s = m_farm->getSolutionStats();
 	string l = m_farm->farmLaunchedFormatted();
-	ss << "Version: " << info->project_version << "\r\n";
-	ss << p << s << ' ' << l << "\r\n";
+	ss <<
+	   "<head><title>Miner Stats</title></head><body><table width=\"50%\" border=1 cellpadding=2 cellspacing=0 align=center>"
+	   "<tr valign=top align=center><th colspan=5>" << info->project_version <<
+	   " on " << hostName << "</th></tr>"
+	   "<tr valign=top align=center>"
+	   "<th>GPU</th><th>Hash Rate (mh/s)</th><th>Temperature (C)</th><th>Fan Percent.</th><th>Power (W)</th></tr>";
+	double hashSum = 0.0;
+	double powerSum = 0.0;
+	for (unsigned i = 0; i < p.minersHashes.size(); i++) {
+		double rate = p.minerRate(p.minersHashes[i]) / 1000000.0;
+		hashSum += rate;
+		ss <<
+		   "<tr valign=top align=center><td>" << i <<
+		   "</td><td>" << fixed << setprecision(2) << rate;
+		if (i < p.minerMonitors.size()) {
+			HwMonitor& hw(p.minerMonitors[i]);
+			powerSum += hw.powerW;
+			ss << "</td><td>" << hw.tempC << "</td><td>" << hw.fanP << "</td><td>" <<
+			   fixed << setprecision(0) << hw.powerW << "</td></tr>";
+		} else
+			ss << "</td><td>-</td><td>-</td><td>-</td></tr>";
+	}
+	ss <<
+	   "<tr valign=top align=center><th>Total</th><td>" <<
+	   fixed << setprecision(2) << hashSum << "</td><td colspan=2>" << s <<
+	   "</td><td>" << fixed << setprecision(0) << powerSum << "</td></tr>"
+	   "</table></body></html>";
 }
 
 static void static_getstat1(stringstream& ss)
@@ -43,7 +73,7 @@ void ev_handler(struct mg_connection* c, int ev, void* p)
 		else {
 			stringstream content;
 			static_getstat1(content);
-			mg_send_head(c, 200, (int)content.str().length(), "Content-Type: text/plain");
+			mg_send_head(c, 200, (int)content.str().length(), "Content-Type: text/html");
 			mg_printf(c, "%.*s", (int)content.str().length(), content.str().c_str());
 		}
 	}
