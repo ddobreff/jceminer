56168b6410a3798b3d6dedc2f2fa9220b9534eaa
Jean Cyr <jean.m.cyr@gmail.com>
2018-03-28 09:16:17 -0400

    Add requisite license notice

--- libapi/http/httpServer.h
+++ libapi/http/httpServer.h
@@ -1,3 +1,7 @@
+/*  Blah, blah, blah.. all this pedantic nonsense to say that this
+    source code is made available under the terms and conditions
+    of the accompanying GNU General Public License */
+
 #pragma once
 
 #include <thread>
