6745b094b82ced309a94bee916657601f932a09d
Jean Cyr <jean.m.cyr@gmail.com>
2018-06-25 15:28:05 -0400

    fix dag gen bug

--- libcuda/ethash_cuda_miner_kernel.cu
+++ libcuda/ethash_cuda_miner_kernel.cu
@@ -31,17 +31,17 @@ ethash_search(
     uint64_t start_nonce
 )
 {
-    uint32_t const gid = blockIdx.x * blockDim.x + threadIdx.x;
-    uint2 mix[4];
-    if (!compute_hash<_PARALLEL_HASH>(start_nonce + gid, d_target, mix))
-        return;
-    if (atomicInc((unsigned*)&g_output->count, 0xffffffff))
-        return;
-    g_output->gid = gid;
-    g_output->mix[0] = devectorize(mix[0]);
-    g_output->mix[1] = devectorize(mix[1]);
-    g_output->mix[2] = devectorize(mix[2]);
-    g_output->mix[3] = devectorize(mix[3]);
+	uint32_t const gid = blockIdx.x * blockDim.x + threadIdx.x;
+	uint2 mix[4];
+	if (!compute_hash<_PARALLEL_HASH>(start_nonce + gid, d_target, mix))
+		return;
+	if (atomicInc((unsigned*)&g_output->count, 0xffffffff))
+		return;
+	g_output->gid = gid;
+	g_output->mix[0] = devectorize(mix[0]);
+	g_output->mix[1] = devectorize(mix[1]);
+	g_output->mix[2] = devectorize(mix[2]);
+	g_output->mix[3] = devectorize(mix[3]);
 }
 
 void run_ethash_search(
@@ -53,14 +53,14 @@ void run_ethash_search(
     uint32_t parallelHash
 )
 {
-    switch (parallelHash) {
-    case 1: ethash_search <1> <<<blocks, threads, 0, stream>>>(g_output, start_nonce); break;
-    case 2: ethash_search <2> <<<blocks, threads, 0, stream>>>(g_output, start_nonce); break;
-    case 8: ethash_search <8> <<<blocks, threads, 0, stream>>>(g_output, start_nonce); break;
-    case 4:
-    default: ethash_search <4> <<<blocks, threads, 0, stream>>>(g_output, start_nonce); break;
-    }
-    CUDA_SAFE_CALL(cudaGetLastError());
+	switch (parallelHash) {
+	case 1: ethash_search <1> <<< blocks, threads, 0, stream>>>(g_output, start_nonce); break;
+	case 2: ethash_search <2> <<< blocks, threads, 0, stream>>>(g_output, start_nonce); break;
+	case 8: ethash_search <8> <<< blocks, threads, 0, stream>>>(g_output, start_nonce); break;
+	case 4:
+	default: ethash_search <4> <<< blocks, threads, 0, stream>>>(g_output, start_nonce); break;
+	}
+	CUDA_SAFE_CALL(cudaGetLastError());
 }
 
 #define ETHASH_DATASET_PARENTS 256
@@ -69,43 +69,46 @@ void run_ethash_search(
 #define shuffl4(_a, _b) __shfl_sync(0xFFFFFFFF, _a, _b, 4)
 
 __global__ void
-ethash_calculate_dag_item(uint32_t start)
+ethash_calculate_dag_item(uint32_t start, bool check)
 {
-    uint32_t const node_index = start + blockIdx.x * blockDim.x + threadIdx.x;
-    if (node_index > d_dag_size * 2) return;
+	uint32_t const node_index = start + blockIdx.x * blockDim.x + threadIdx.x;
+	if (node_index >= d_dag_size * 2) return;
 
-    hash200_t dag_node;
-    copy(dag_node.uint4s, d_light[node_index % d_light_size].uint4s, 4);
-    dag_node.words[0] ^= node_index;
-    SHA3_512(dag_node.uint2s);
+	hash200_t dag_node;
+	copy(dag_node.uint4s, d_light[node_index % d_light_size].uint4s, 4);
+	dag_node.words[0] ^= node_index;
+	SHA3_512(dag_node.uint2s);
 
-    const int thread_id = threadIdx.x & 3;
+	const int thread_id = threadIdx.x & 3;
 
-    for (uint32_t i = 0; i != ETHASH_DATASET_PARENTS; ++i) {
-        uint32_t parent_index = fnv(node_index ^ i, dag_node.words[i % NODE_WORDS]) % d_light_size;
-        for (uint32_t t = 0; t < 4; t++) {
+	for (uint32_t i = 0; i != ETHASH_DATASET_PARENTS; ++i) {
+		uint32_t parent_index = fnv(node_index ^ i, dag_node.words[i % NODE_WORDS]) % d_light_size;
+		for (uint32_t t = 0; t < 4; t++) {
 
-            uint32_t shuffle_index = shuffl4(parent_index, t);
+			uint32_t shuffle_index = shuffl4(parent_index, t);
 
-            uint4 p4 = d_light[shuffle_index].uint4s[thread_id];
-            for (int w = 0; w < 4; w++) {
-                uint4 s4 = make_uint4(shuffl4(p4.x, w), shuffl4(p4.y, w), shuffl4(p4.z, w), shuffl4(p4.w, w));
-                if (t == thread_id)
-                    dag_node.uint4s[w] = fnv4(dag_node.uint4s[w], s4);
-            }
-        }
-    }
-    SHA3_512(dag_node.uint2s);
-    hash64_t* dag_nodes = (hash64_t*)d_dag;
+			uint4 p4 = d_light[shuffle_index].uint4s[thread_id];
+			for (int w = 0; w < 4; w++) {
+				uint4 s4 = make_uint4(shuffl4(p4.x, w), shuffl4(p4.y, w), shuffl4(p4.z, w), shuffl4(p4.w, w));
+				if (t == thread_id)
+					dag_node.uint4s[w] = fnv4(dag_node.uint4s[w], s4);
+			}
+		}
+	}
+	SHA3_512(dag_node.uint2s);
+	hash64_t* dag_nodes = (hash64_t*)d_dag;
 
-    for (uint32_t t = 0; t < 4; t++) {
-        uint32_t shuffle_index = shuffl4(node_index, t);
-        uint4 s[4];
-        for (uint32_t w = 0; w < 4; w++)
-            s[w] = make_uint4(shuffl4(dag_node.uint4s[w].x, t), shuffl4(dag_node.uint4s[w].y, t), shuffl4(dag_node.uint4s[w].z, t),
-                              shuffl4(dag_node.uint4s[w].w, t));
-        dag_nodes[shuffle_index].uint4s[thread_id] = s[thread_id];
-    }
+	for (uint32_t t = 0; t < 4; t++) {
+		uint32_t shuffle_index = shuffl4(node_index, t);
+		uint4 s[4];
+		for (uint32_t w = 0; w < 4; w++)
+			s[w] = make_uint4(shuffl4(dag_node.uint4s[w].x, t), shuffl4(dag_node.uint4s[w].y, t), shuffl4(dag_node.uint4s[w].z, t),
+			                  shuffl4(dag_node.uint4s[w].w, t));
+		if (!check)
+			dag_nodes[shuffle_index].uint4s[thread_id] = s[thread_id];
+		else if (!(shuffle_index == 0 && thread_id < 2))
+			dag_nodes[shuffle_index].uint4s[thread_id] = s[thread_id];
+	}
 }
 
 void ethash_generate_dag(
@@ -115,13 +118,13 @@ void ethash_generate_dag(
     cudaStream_t stream
 )
 {
-    const uint32_t work = (uint32_t)(dag_size / sizeof(hash64_t));
-    const uint32_t chunk = blocks * threads;
-    for (uint32_t i = 0; i < work; i += chunk) {
-        ethash_calculate_dag_item <<<blocks, threads, 0, stream>>>(i);
-        CUDA_SAFE_CALL(cudaStreamSynchronize(stream));
-    }
-    CUDA_SAFE_CALL(cudaGetLastError());
+	const uint32_t work = (uint32_t)(dag_size / sizeof(hash64_t));
+	const uint32_t chunk = blocks * threads;
+	for (uint32_t i = 0; i < work; i += chunk) {
+		ethash_calculate_dag_item <<< blocks, threads, 0, stream>>>(i, i != 0);
+		CUDA_SAFE_CALL(cudaStreamSynchronize(stream));
+	}
+	CUDA_SAFE_CALL(cudaGetLastError());
 }
 
 void set_constants(
@@ -131,10 +134,10 @@ void set_constants(
     uint32_t _light_size
 )
 {
-    CUDA_SAFE_CALL(cudaMemcpyToSymbol(d_dag, &_dag, sizeof(hash128_t*)));
-    CUDA_SAFE_CALL(cudaMemcpyToSymbol(d_dag_size, &_dag_size, sizeof(uint32_t)));
-    CUDA_SAFE_CALL(cudaMemcpyToSymbol(d_light, &_light, sizeof(hash64_t*)));
-    CUDA_SAFE_CALL(cudaMemcpyToSymbol(d_light_size, &_light_size, sizeof(uint32_t)));
+	CUDA_SAFE_CALL(cudaMemcpyToSymbol(d_dag, &_dag, sizeof(hash128_t*)));
+	CUDA_SAFE_CALL(cudaMemcpyToSymbol(d_dag_size, &_dag_size, sizeof(uint32_t)));
+	CUDA_SAFE_CALL(cudaMemcpyToSymbol(d_light, &_light, sizeof(hash64_t*)));
+	CUDA_SAFE_CALL(cudaMemcpyToSymbol(d_light_size, &_light_size, sizeof(uint32_t)));
 }
 
 void set_header_and_target(
@@ -142,7 +145,7 @@ void set_header_and_target(
     uint64_t _target
 )
 {
-    CUDA_SAFE_CALL(cudaMemcpyToSymbol(d_header, &_header, sizeof(hash32_t)));
-    CUDA_SAFE_CALL(cudaMemcpyToSymbol(d_target, &_target, sizeof(uint64_t)));
+	CUDA_SAFE_CALL(cudaMemcpyToSymbol(d_header, &_header, sizeof(hash32_t)));
+	CUDA_SAFE_CALL(cudaMemcpyToSymbol(d_target, &_target, sizeof(uint64_t)));
 }
 
